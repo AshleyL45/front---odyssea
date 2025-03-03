dccd617b12f215f65701234aaee7fe4b1ec10d8c
ckenza <kenza.chrifi36@gmail.com>
2025-03-03 15:45:46 +0100

    done -> NavbarDashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import CustomButton from "./components/ReusableComponents/CustomButton";
+import NavbarDashboard from "./components/NavbarDashboard";
 
 function App() {
   return (
     <>
-        <Navbar/>
-
-        <Footer />
+        <NavbarDashboard/>
 
     </>
   );
---
+++ src/components/NavbarDashboard.tsx
@@ -0,0 +1,33 @@
+import {FC} from 'react';
+import logo from "../images/logo_name.png";
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+
+const NavbarDashboard: FC<{}> = ({}) => {
+    return (
+        <>
+
+            <nav className="navbar-dashboard">
+
+                <div>
+                    <a href="#" style={{display: "flex", alignItems: "center", textDecoration: "underline"}}>
+                        <ArrowBackIosNewIcon sx={{fontSize: "12px"}}/>
+                        retour au site
+                    </a>
+                </div>
+
+                <a href="#"><img className="logo" src={logo} alt="Odyssea logo"/></a>
+
+                <div style={{border: "solid 2px black", borderRadius:7, padding:"7px 10px"}}>
+                    <a href="#">
+                        <p>Besoin d'aide ?</p>
+                    </a>
+                </div>
+
+            </nav>
+
+
+        </>
+    );
+};
+
+export default NavbarDashboard;
